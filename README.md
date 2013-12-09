321020208113e3f0780bf459d68b5739ffffbe11
MaChao <zjcn5205@163.com>
2013-12-09 18:45:04 +0800

    v1.0
    提交第一版代码

---
+++ README.md
@@ -0,0 +1,23 @@
+JSON
+============
+
+为低版本浏览器增加JSON接口，支持JSON.stringify和JSON.parse简单调用
+
+**支持浏览器**
+
+	IE6 / IE7 / IE8 / IE9 / IE10 / IE11 / FireFox / Chrome / Opera / Safri
+
+**使用方法**
+
+在head标签内引入json.js即可。
+
+	<script src='json.js'></script>
+**接口示例**
+
+	var str  = window.JSON.stringify({name:"zjcn5205"});     //序列化
+	var data = window.JSON.parse("{\"name\":\"zjcn5205\"}"); //格式化
+
+ **补充说明**
+
+增加的 window.JSON 的两个接口仅仅支持一个参数，虽不是完整的接口实现，但已经可以满足日常开发需要。如果想使用更完备的模拟组件，请移步：[https://github.com/machao/JSON-js](https://github.com/machao/JSON-js "https://github.com/machao/JSON-js")
+
---
+++ json.js
@@ -0,0 +1,142 @@
+/*!
+ * JSON 组件
+ *
+ * 版权所有(C) 2013 马超 (zjcn5205@yeah.net)
+ *
+ * 这一程序是自由软件，你可以遵照自由软件基金会出版的GNU通用公共许可证条款来修改和重新发布
+ * 这一程序。或者用许可证的第二版，或者（根据你的选择）用任何更新的版本。
+ * 发布这一程序的目的是希望它有用，但没有任何担保。甚至没有适合特定目的的隐含的担保。更详细
+ * 的情况请参阅GNU通用公共许可证。
+ * 你应该已经和程序一起收到一份GNU通用公共许可证的副本。如果还没有，写信给：
+ * The Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA
+ */
+/*******************************************************************************
+ * 2012-05-10 马超 创建
+ * 检查window.JSON是否存在，不存在则创建对象
+ */
+(function(window){
+/*
+ * 如果有系统默认的JSON对象，则不替换
+ */
+if( window.JSON )
+	return;
+/*
+ * 工具函数
+ * 移植于Baidu前端开源 Tangram 库
+ */
+var escapeMap = {
+	"\b": '\\b',
+	"\t": '\\t',
+	"\n": '\\n',
+	"\f": '\\f',
+	"\r": '\\r',
+	'"' : '\\"',
+	"\\": '\\\\'
+},		
+/* 字符串序列化 */
+encodeString = function(source) {
+	if (/["\\\x00-\x1f]/.test(source)) {
+		source = source.replace(
+			/["\\\x00-\x1f]/g, 
+			function (match) {
+				var c = escapeMap[match];
+				if (c) {
+					return c;
+				}
+				c = match.charCodeAt();
+				return "\\u00" 
+						+ Math.floor(c / 16).toString(16) 
+						+ (c % 16).toString(16);
+			});
+	}
+	return '"' + source + '"';
+},
+/* 数组序列化 */
+encodeArray = function(source) {
+	var result = ["["], 
+		l = source.length,
+		preComma, i, item;
+		
+	for (i = 0; i < l; i++) {
+		item = source[i];
+		
+		switch (typeof item) {
+		case "undefined":
+		case "function":
+		case "unknown":
+			break;
+		default:
+			if(preComma) {
+				result.push(',');
+			}
+			result.push(encode(item));
+			preComma = 1;
+		}
+	}
+	result.push("]");
+	return result.join("");
+},
+/* 处理日期序列化时的补零 */
+pad = function(source) {
+	return source < 10 ? '0' + source : source;
+},
+/* 日期序列化 */
+encodeDate = function(source){
+	return '"' + source.getUTCFullYear() + "-" 
+			+ pad(source.getUTCMonth() + 1) + "-" 
+			+ pad(source.getUTCDate()) + "T" 
+			+ pad(source.getUTCHours()) + ":" 
+			+ pad(source.getUTCMinutes()) + ":" 
+			+ pad(source.getUTCSeconds()) + '"';
+},
+/* 对象序列化 */
+hasOwn = Object.prototype.hasOwnProperty,
+encodeObj = function( value ){
+	var result = ['{'], preComma, item;
+	for (var key in value) {
+		if (hasOwn.call(value, key)) {
+			item = value[key];
+			switch (typeof item) {
+			case 'undefined':
+			case 'unknown':
+			case 'function':
+				break;
+			default:
+				preComma && result.push(',');
+				preComma = 1;
+				result.push(encode(key) + ':' + encode(item));
+			}
+		}
+	}
+	result.push('}');
+	return result.join('');
+},
+/* 序列化接口函数 */
+encode = function (value) {
+	switch (typeof value) {
+		case 'undefined':
+			return 'undefined';
+		case 'number':
+			return isFinite(value) ? String(value) : "null";
+		case 'string':
+			return encodeString(value);
+		case 'boolean':
+			return String(value);
+		default:
+			return value === null ? 'null'
+				: value instanceof Array ? encodeArray(value)
+				: value instanceof Date ? encodeDate(value)
+				: encodeObj(value);
+	}
+};
+/*
+ * 创建JSON对象
+ */
+window.JSON = {
+	parse : function(data){//宽容格式解决方案
+		data = data.replace(/("|')\\?\/Date\((-?[0-9+]+)\)\\?\/\1/g, "new Date($2)");
+		return (new Function("return " + data))();
+	},
+	stringify : function(v){ return encode(v); }
+};
+})(window);
